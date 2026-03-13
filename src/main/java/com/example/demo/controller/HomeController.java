08b0785dd8bcdd8b2fe2cf0c7cd746ac008b6959
dum dum <sugardreamjelly@gmail.com>
2026-03-12 23:03:17 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";  
+    }
+    
+    @GetMapping("/about")
+    public String about() {
+    	return "about";
+    }
+    
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+}
